046707663bf78bf15bc57863793c21b366d90396
Gregory Hoyl Jr <ghjunior@gmail.com>
2012-02-11 23:00:50 +1300

    init

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+/*var neo4j = require('neo4j');
+var db = new neo4j.GraphDatabase('http://localhost:7474');
+
+db.getNodeById(1, function(err, res) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(res);
+  }
+});
+
+db.getRelationshipById(1, function(err, res) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(res);
+  }
+});
+
+var query = "START ghjunior=node:node_auto_index(username = 'ghjunior') \
+RETURN ghjunior";
+
+db.query(function(err, res) { console.log(res) }, query);*/
